16973eefd0dbdf1ac6041fd19686abea4f21625d
Lenn Perik <lennperik@hotmail.nl>
2023-09-07 13:55:35 +0000

    Refresh disconnect timer before end position exception

--- motionblinds_ble/device.py
+++ motionblinds_ble/device.py
@@ -53,6 +53,7 @@ def requires_end_positions(func: Callable) -> Callable:
             and not self.end_position_info.UP
             and not ignore_end_positions_not_set
         ):
+            self.refresh_disconnect_timer()
             raise NoEndPositionsException(
                 EXCEPTION_NO_END_POSITIONS.format(device_name=self.device_name)
             )
@@ -68,6 +69,7 @@ def requires_favorite_position(func: Callable) -> Callable:
             and not self.end_position_info.UP
             and not self.end_position_info.FAVORITE
         ):
+            self.refresh_disconnect_timer()
             raise NoFavoritePositionException(
                 EXCEPTION_NO_FAVORITE_POSITION.format(device_name=self.device_name)
             )
